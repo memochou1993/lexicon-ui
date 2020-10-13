0c7b2ca7b903300788154cf550f86cdc5f9ace09
Memo Chou <memochou1993@gmail.com>
2020-10-14 00:31:03 +0800

    Update lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,19 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      vue: 'never',
-    }],
+    'max-len': [
+      'error',
+      {
+        'code': 120,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
   },
 };
