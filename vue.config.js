6c6d43a1b3cddfcce8c039bf9206e61d11794fed
Memo Chou <memochou1993@gmail.com>
2020-12-27 19:07:38 +0800

    Dockerize app

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  outputDir: '../../public',
-  indexPath: process.env.NODE_ENV === 'production'
-    ? '../resources/views/app.blade.php'
-    : 'index.html',
   transpileDependencies: [
     'vuetify',
   ],
